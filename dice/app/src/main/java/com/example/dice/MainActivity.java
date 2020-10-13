cc7ccff839e03b141dbe9dab1e48dcce907f0b47
Discoonect <67423146+Discoonect@users.noreply.github.com>
2020-10-13 11:54:02 +0900

    주사위 루프

--- dice/app/src/main/java/com/example/dice/MainActivity.java
+++ dice/app/src/main/java/com/example/dice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int num1 = 0;
+        int num2 = 0;
+        while(true){
+            // 1. 주사위를 두번 던집니다.
+            num1 = (int)(Math.random() * 6) + 1 ;
+            num2 = (int)(Math.random() * 6) + 1 ;
+            // 2. 주사위 두개 값을 화면에 (값1, 값2) 출력
+            Log.i("aaa",("("+num1+", "+num2+")"));
+            // 3. 주사위 두개의 합이 5이면, 이 반복문을 빠져 나가게 코드 작성
+
+
+            if( (num1+num2) == 5  ) {
+                break;
+            }
+        }
     }
 }
